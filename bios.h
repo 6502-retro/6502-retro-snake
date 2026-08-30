47e95599b0f6a47394432552c908b8ea83fb93e3
productiondave <kiwilathams@gmail.com>
2026-08-31 00:32:49 +1200

    remove duplicated declarations in bios.h, add compile_flags.txt for clangd linter

--- bios.h
+++ bios.h
@@ -1,6 +1,7 @@
 // vim: set et ts=4 sw=4
 #ifndef BIOS_H
 #define BIOS_H
+#include <stdint.h>
 
 extern void __fastcall__ bios_conout(char c);
 extern uint8_t bios_conin();
@@ -16,10 +17,6 @@ extern void __fastcall__ bios_sn_send(uint8_t b);
 extern void bios_led_on();
 extern void bios_led_off();
 extern uint8_t bios_get_button();
-
-extern void bios_sn_start();
-extern void bios_sn_stop();
-extern void bios_sn_send();
 
 extern uint8_t notectr;
 #endif
